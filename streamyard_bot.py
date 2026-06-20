8ce12f68b787c9f64b195d9908be8f90c44345d7
faizshayar06-spec <faizshayar06@gmail.com>
2026-06-20 21:44:19 +0530

    Update streamyard_bot.py

--- streamyard_bot.py
+++ streamyard_bot.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 # --- CONFIG ---
-GUEST_URL = "https://streamyard.com/dmbfjxek7d" 
+GUEST_URL = "https://streamyard.com/s7hdqjcdfw" 
 STREAM_KEY = os.getenv("YT_STREAM_KEY")
 
 def start_stream():
